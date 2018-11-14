c05c1b4408a18611a2d808f9122b06058f3816b4
John Doe <leandro.wauters@gmail.com>
2018-11-14 14:25:15 -0500

    First Draft

---
+++ Autolaying Practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Autolaying Practice
+//
+//  Created by Leandro Wauters on 11/14/18.
+//  Copyright © 2018 Leandro Wauters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
